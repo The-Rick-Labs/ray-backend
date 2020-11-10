6cd6e811a091c4eeab4a9db5b5d51c80185a6318
Abdur Javaid <47406594+abdurj@users.noreply.github.com>
2020-11-10 14:52:00 -0500

    arduino integration

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express')
-
 app = express()
+let SerialPort = require('serialport');
+
+let sp = new SerialPort('COM3', {baudRate: 9600});
+sp.on('open', ()=>{
+	console.log("Opened Serial Port");
+});
+
 
 app.get('/smiley', (req, res) => {
 	console.log('smiley')
-	res.send(':]')
+	sp.write('h')
+    res.send(':]')
+    
 })
+
 app.get('/sad', (req, res) => {
+	sp.write('s')
 	console.log('sad')
 	res.send('[:')
 })
 app.get('/uwu', (req, res) => {
+	sp.write('b')
 	console.log('UwU')
 	res.send('UwU')
 }).listen(8080, () => {
