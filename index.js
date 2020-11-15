d393981406596bb4a5a274b67b1aa14000ff77ae
Abdur Javaid <marjavai@uwaterloo.ca>
2020-11-15 16:14:17 -0500

    pre-merge

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require('express')
 var cors = require('cors')
 
 app = express()
+app.use(cors())
 let SerialPort = require('serialport')
 
 let sp = new SerialPort('COM3', { baudRate: 9600 })
 sp.on('open', () => {
 	console.log('Opened Serial Port')
 })
-
-app.use(cors())
 
 app.get('/smiley', (req, res) => {
 	console.log('smiley')
