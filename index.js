7f79f2e3c2d32635518d9e56ab98f48e7fb279a9
Bilal Khan <bilal.khan1@uwaterloo.ca>
2020-12-04 20:26:51 -0600

    add pulling from gcal backend

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express')
+const readline = require('readline')
 var cors = require('cors')
+const util = require('util')
+
+const fs = require('fs')
+const { google } = require('googleapis')
+const { json } = require('express')
+const { list } = require('serialport')
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+const TOKEN_PATH = 'token.json'
 
 app = express()
 app.use(cors())
@@ -8,6 +17,47 @@ let SerialPort = require('serialport')
 let sp = new SerialPort('COM3', { baudRate: 9600 })
 sp.on('open', () => {
 	console.log('Opened Serial Port')
+})
+
+app.get('/calendar', (req, response) => {
+	const readfile = util.promisify(fs.readFile)
+
+	const client_secret = 'Xmb0-96Q00ycZhrXWJ1igt5q'
+	const client_id = '363244684321-6tnu9aq351rpofbkqnvamhdg8jo8ntn0.apps.googleusercontent.com'
+	const redirect_uris = 'http://localhost:3000'
+	const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris)
+	// Check if we have previously stored a token.
+
+	readfile('token.json')
+		.then((data) => {
+			return JSON.parse(data)
+		})
+		.then(async (data) => {
+			await oAuth2Client.setCredentials(data)
+			return oAuth2Client
+		})
+		.then((auth) => {
+			const calendar = google.calendar({ version: 'v3', auth })
+			calendar.events.list(
+				{
+					calendarId: 'primary',
+					timeMin: new Date().toISOString(),
+					maxResults: 10,
+					singleEvents: true,
+					orderBy: 'startTime',
+				},
+				(err, res) => {
+					if (err) return console.log('The API returned an error: ' + err)
+					const events = res.data.items
+					// console.log(events)
+					response.send({ items: events })
+					// temp(events)
+					// return events
+				}
+			)
+		})
+
+	// response.send('hello')
 })
 
 app.get('/smiley', (req, res) => {
